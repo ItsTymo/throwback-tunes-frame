2e1306c24406bb97426e0784f1f015cb1623ff2b
ItsTymo <itstymo.eth@gmail.com>
2025-04-08 22:51:17 -0700

    Fix tsconfig/import issues for build

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { WagmiProvider } from "wagmi";
+import { config } from "./wagmi.js";
 
-import App from "./App.tsx";
-import { config } from "./wagmi.ts";
+import App from "./App.js";
+import "./wagmi.js";
 
 import "./index.css";
 
